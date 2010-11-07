f1054c2f6b7ace8b5f7d8d8d4165682240a02a22
andrewbuck40 <andrewbuck40@users.sourceforge.net>
2010-11-07 01:05:33 -0500

    More work on the trap classes.

--- include/TrapCannon.h
+++ include/TrapCannon.h
@@ -1,8 +1,36 @@
 #ifndef TRAPCANNON_H
 #define TRAPCANNON_H
 
+#include <vector>
+#include <string>
+
+#include "Trap.h"
+#include "Tile.h"
+#include "AttackableObject.h"
+
 class TrapCannon : public Trap
 {
+	public:
+		TrapCannon();
+
+		void addCoveredTile(Tile* t, double nHP = defaultTileHP);
+		void removeCoveredTile(Tile* t);
+		Tile* getCoveredTile(int index);
+		std::vector<Tile*> getCoveredTiles();
+		unsigned int numCoveredTiles();
+		void clearCoveredTiles();
+
+		// Methods inherited from AttackableObject.
+		//TODO:  Sort these into the proper places in the rest of the file.
+		double getHP(Tile *tile);
+		double getDefense();
+		void takeDamage(double damage, Tile *tileTakingDamage);
+		void recieveExp(double experience);
+		bool isMobile();
+		int getLevel();
+		int getColor();
+		string getName();
+		AttackableObject::AttackableObjectType getAttackableObjectType();
 };
 
 #endif
--- src/TrapCannon.cpp
+++ src/TrapCannon.cpp
@@ -1,1 +1,53 @@
 #include "TrapCannon.h"
+
+TrapCannon::TrapCannon()
+	: Trap()
+{
+}
+
+double TrapCannon::getHP(Tile *tile)
+{
+	return 0;
+}
+
+double TrapCannon::getDefense()
+{
+	return 0;
+}
+
+void TrapCannon::takeDamage(double damage, Tile *tileTakingDamage)
+{
+}
+
+void TrapCannon::recieveExp(double experience)
+{
+}
+
+bool TrapCannon::isMobile()
+{
+	return false;
+}
+
+int TrapCannon::getLevel()
+{
+	return 1;
+}
+
+int TrapCannon::getColor()
+{
+	if(controllingSeat != NULL)
+		return controllingSeat->color;
+	else
+		return 0;
+}
+
+string TrapCannon::getName()
+{
+	return name;
+}
+
+AttackableObject::AttackableObjectType TrapCannon::getAttackableObjectType()
+{
+	return trap;
+}
+
