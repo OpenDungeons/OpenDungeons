151002dcfa1fb0ffb2bb377e5043115429592f51
StefanP.MUC <StefanP.MUC@foo.bar>
2012-03-18 10:25:43 +0100

    make ActiveEntity explicitly pure virtual because it was already used implicitly this way

--- source/ActiveEntity.cpp
+++
@@ -1,7 +0,0 @@
-#include "ActiveEntity.h"
-
-bool ActiveEntity::doUpkeep()
-{
-    return true;
-}
-
--- source/ActiveEntity.h
+++ source/ActiveEntity.h
@@ -1,13 +1,26 @@
+/*!
+ * \file   ActiveEntity.h
+ * \date
+ * \author
+ * \brief  Pure virtual base class to make an object active
+ */
+
 #ifndef ACTIVEENTITY_H
 #define ACTIVEENTITY_H
 
+/*! \class ActiveEntity ActiveEntity.h
+ *  \brief Holds everything common to active objects (that do something on their own)
+ *
+ * Functions and properties that are needed by all active objects should be placed in here.
+ * An object is active if it is doing something on it's own (like Rooms and Traps).
+ */
 class ActiveEntity
 {
     public:
         virtual ~ActiveEntity() {}
 
-        virtual bool doUpkeep();
+        //! \brief defines what happens on each turn with this object
+        virtual bool doUpkeep() = 0;
 };
 
 #endif
-
