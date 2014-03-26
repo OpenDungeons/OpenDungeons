6d3015738d01d8a5c54ebc9ae59b5f8323c2c3c0
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-03-26 23:30:26 +0100

    Added a copyright header to the Helper.h file.

--- source/Helper.h
+++ source/Helper.h
@@ -3,6 +3,21 @@
  * \date   10 September 2011
  * \author StefanP.MUC
  * \brief  Provides helper functions, constants and defines
+ *
+ *  Copyright (C) 2011-2014  OpenDungeons Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef HELPER_H_
@@ -46,4 +61,4 @@ namespace Helper
     }
 }
 
-#endif /* HELPER_H_ */
+#endif // HELPER_H_
