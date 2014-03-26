256929f3a6853c601d1b048889f8934b15e1c833
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-03-26 22:22:47 +0100

    Removed the dead and duplicate code put in source/Render_Request/*
    For now, Everything is in the RenderManager.cpp file.
    
    As Paul pointed out, we could later turn each render requests into
    specialized classes, but we should avoid putting dead code anyway.

--- source/Render_Request/rrScaleSceneNode.cpp
+++
@@ -1,9 +0,0 @@
-void RenderManager::rrScaleSceneNode( const RenderRequest& renderRequest )
-{
-    Ogre::SceneNode* node = static_cast<Ogre::SceneNode*>(renderRequest.p);
-
-    if (node != NULL)
-    {
-        node->scale(renderRequest.vec);
-    }
-}
