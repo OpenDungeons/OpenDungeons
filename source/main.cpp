ee2e8eda3627e3ae03baf3963485d1c5fec134b0
Tomasz Luchowski <paul424@o2.pl>
2014-06-18 01:39:53 +0200

    Modified main.cpp to print DEMANGLED CPP stacktrace on various system signals , requires include header <cxxabi.h> . Need some work for i386 and x86-64 to display the top frame ( EIP etc)

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,116 @@
 #include <iostream>
 
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+#include <cxxabi.h>
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
 #include "ODApplication.h"
 #include "Socket.h"
 
+
+
+/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
+typedef struct _sig_ucontext {
+    unsigned long     uc_flags;
+    struct ucontext   *uc_link;
+    stack_t           uc_stack;
+    struct sigcontext uc_mcontext;
+    sigset_t          uc_sigmask;
+} sig_ucontext_t;
+
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext)
+{
+    sig_ucontext_t * uc = (sig_ucontext_t *)ucontext;
+
+    // void * caller_address = (void *) uc->uc_mcontext.eip; // x86 specific
+
+    std::cerr << "signal " << sig_num 
+              << " (" << strsignal(sig_num) << "), address is " 
+              << info->si_addr << " from " // << caller_address 
+              << std::endl << std::endl;
+
+    void * array[50];
+    int size = backtrace(array, 50);
+
+    array[1] = (void*)999999; // caller_address;
+
+    char ** messages = backtrace_symbols(array, size);    
+
+    // skip first stack frame (points here)
+    for (int i = 1; i < size && messages != NULL; ++i)
+    {
+        char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
+
+        // find parantheses and +address offset surrounding mangled name
+        for (char *p = messages[i]; *p; ++p)
+        {
+            if (*p == '(') 
+            {
+                mangled_name = p; 
+            }
+            else if (*p == '+') 
+            {
+                offset_begin = p;
+            }
+            else if (*p == ')')
+            {
+                offset_end = p;
+                break;
+            }
+        }
+
+        // if the line could be processed, attempt to demangle the symbol
+        if (mangled_name && offset_begin && offset_end && 
+            mangled_name < offset_begin)
+        {
+            *mangled_name++ = '\0';
+            *offset_begin++ = '\0';
+            *offset_end++ = '\0';
+
+            int status;
+            char * real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
+
+            // if demangling is successful, output the demangled function name
+            if (status == 0)
+            {    
+                std::cerr << "[bt]: (" << i << ") " << messages[i] << " : " 
+                          << real_name << "+" << offset_begin << offset_end 
+                          << std::endl;
+
+            }
+            // otherwise, output the mangled function name
+            else
+            {
+                std::cerr << "[bt]: (" << i << ") " << messages[i] << " : " 
+                          << mangled_name << "+" << offset_begin << offset_end 
+                          << std::endl;
+            }
+            free(real_name);
+        }
+        // otherwise, print the whole line
+        else
+        {
+            std::cerr << "[bt]: (" << i << ") " << messages[i] << std::endl;
+        }
+    }
+    std::cerr << std::endl;
+
+    free(messages);
+
+    exit(EXIT_FAILURE);
+}
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -22,6 +130,19 @@ int main(int argc, char **argv)
         exit(1);
     }
 #endif
+struct sigaction sigact;
+
+sigact.sa_sigaction = crit_err_hdlr;
+sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+if (sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL) != 0)
+{
+fprintf(stderr, "error setting signal handler for %d (%s)\n",
+	    SIGSEGV, strsignal(SIGSEGV));
+
+exit(EXIT_FAILURE);
+}
+
     try
     {
         new ODApplication;
