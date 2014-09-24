c66290226907a22adc2bc997e0ff324b42b8af53
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-09-25 01:11:30 +0200

    Added the new light indicator and fixed how light indicators are handled.
    You can see one in the skirmish oldTest map.
    
    Fixes #168

--- source/Console_executePromptCommand.cpp
+++ source/Console_executePromptCommand.cpp
@@ -732,9 +732,6 @@ bool Console::executePromptCommand(const std::string& command, std::string argum
                     std::string ip;
                     tempSS >> ip;
 
-                    // Destroy the meshes associated with the map lights that allow you to see/drag them in the map editor.
-                    gameMap->clearMapLightIndicators();
-
                     if (ODClient::getSingleton().connect(ip, ODApplication::PORT_NUMBER))
                     {
                         frameListener->mCommandOutput += "\nConnection successful.\n";
