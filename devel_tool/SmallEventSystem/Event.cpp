cd8ae47bd06fe7e4540d8bceb02cc093219f0b71
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-11-03 21:44:35 +0100

    Took care of #276
    The old map files have been moved to the media repo in another PR.

--- devel_tool/SmallEventSystem/Event.cpp
+++
@@ -1,9 +0,0 @@
-#include "Event.h"
-using std::string;
-using std::list;
-
-Event::Event(){}
-Event::Event(const list<string> &ll):actionList(ll){}
-Event::~Event(){}
-list<string> Event::getActionList(){}
-bool  Event::setActionList(const list<string> &ll){ actionList = ll;}
