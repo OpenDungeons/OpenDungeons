1883eafd366b813c72b1b29601a9b56265f30079
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-11-13 16:24:12 +0100

    Made the keyboard grab option a linux option only.
    And properly defined it through CMake Cxx Flags.
    
    Thanks to @akien-mga for pointing out.

--- source/modes/InputManager.cpp
+++ source/modes/InputManager.cpp
@@ -58,7 +58,7 @@ InputManager::InputManager(Ogre::RenderWindow* renderWindow):
 #elif defined OIS_LINUX_PLATFORM
     paramList.insert(std::make_pair(std::string("x11_mouse_grab"), std::string("false")));
     paramList.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("false")));
-#if defined OD_NO_KEYBOARD_GRAB
+#if defined OD_LINUX_NO_KEYBOARD_GRAB
     paramList.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("false")));
 #else
     paramList.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("true")));
