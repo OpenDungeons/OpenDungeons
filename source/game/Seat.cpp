1a1bfc71b223bbd0fb05bc73944de4da616f3eab
hwoarangmy <hwoarangmy@hotmail.com>
2015-05-16 14:28:57 +0200

    Fixed failing assert in Seat

--- source/game/Seat.cpp
+++ source/game/Seat.cpp
@@ -75,7 +75,7 @@ void Seat::setTeamId(int teamId)
     OD_ASSERT_TRUE_MSG(std::find(mAvailableTeamIds.begin(), mAvailableTeamIds.end(),
         teamId) != mAvailableTeamIds.end(), "Unknown team id=" + Ogre::StringConverter::toString(teamId)
         + ", for seat id=" + Ogre::StringConverter::toString(getId()));
-    OD_ASSERT_TRUE_MSG(teamId != 0, "Invalid rogue team id for seat id=" + Helper::toString(getId()));
+    OD_ASSERT_TRUE_MSG(teamId != 0 || isRogueSeat(), "Invalid rogue team id for seat id=" + Helper::toString(getId()));
     mTeamId = teamId;
 }
 
