43fa9a77900b0ae3bd675607643137e87a3bdcfa
hwoarangmy <hwoarangmy@hotmail.com>
2016-01-19 20:14:50 +0100

    Fixed random segfault when a room absorbs another while creatures are using it
    fixes #1084

--- source/rooms/Room.cpp
+++ source/rooms/Room.cpp
@@ -102,9 +102,13 @@ void Room::absorbRoom(Room *r)
     mNumActiveSpots += r->mNumActiveSpots;
 
     // Every creature working in this room should go to the new one (this is used in the server map only)
-    // To do that, we clear the creatures using the room actions and we push forced
-    // search job/eat
-    for(Creature* creature : r->mCreaturesUsingRoom)
+    // To do that, we notify the new room that a creature was using an absorbed one. It is
+    // the room responsability to handle correctly the creatures (making them stop to use the old
+    // room if needed)
+    // Note that we iterate a copy of r->mCreaturesUsingRoom because handleCreatureUsingAbsorbedRoom
+    // will want to make the creature stop using the room and it may break the iteration
+    std::vector<Creature*> creaturesUsingRoom = r->mCreaturesUsingRoom;
+    for(Creature* creature : creaturesUsingRoom)
         handleCreatureUsingAbsorbedRoom(*creature);
 
     mCreaturesUsingRoom.insert(mCreaturesUsingRoom.end(), r->mCreaturesUsingRoom.begin(), r->mCreaturesUsingRoom.end());
