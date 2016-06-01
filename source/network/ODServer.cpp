819123d45bb6a4fb2b3cd0adcd62d5b0d61c2d90
hwoarangmy <hwoarangmy@hotmail.com>
2016-06-01 23:26:24 +0200

    Refresh entities visible tiles at turn start instead of end
    That allows entities added with console commands to not have a turn with empty visible tiles

--- source/network/ODServer.cpp
+++ source/network/ODServer.cpp
@@ -364,6 +364,7 @@ void ODServer::startNewTurn(double timeSinceLastTurn)
         }
     }
 
+    gameMap->updateVisibleEntities();
     switch(mServerMode)
     {
         case ServerMode::ModeGameSinglePlayer:
@@ -385,7 +386,6 @@ void ODServer::startNewTurn(double timeSinceLastTurn)
             break;
     }
 
-    gameMap->updateVisibleEntities();
     gameMap->processDeletionQueues();
 }
 
